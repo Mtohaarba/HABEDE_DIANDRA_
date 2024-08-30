199a3acd27c86c77cf4849b17b09c097091fed1b
Mtohaarba <arbaarba749@gmail.com>
2024-08-30 17:00:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    var message = document.getElementById("specialMessage");
+    message.style.display = "block";
+}
+
+
+function playAudio() {
+    var audio = document.getElementById('myAudio');
+    audio.play();
+}
